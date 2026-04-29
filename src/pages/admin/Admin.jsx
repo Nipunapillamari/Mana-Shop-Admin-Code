3471611592ee0fc468f480bd3d427df0eb87e88f
Nipunapillamari <nipunapillamari@gmail.com>
2026-04-29 22:42:54 +0530

    update side bar added mobile reponsive

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,34 +1,18 @@
-import React from 'react'
-import "./Admin.css"
-import Sidebar from '../../components/sidebar/Sidebar'
-import {Routes,Route} from "react-router-dom"
-import AddProduct from "../../components/addproduct/AddProduct"
-import ListProduct from "../../components/listproduct/ListProduct"
-import Dashboard from "../../components/dashboard/Dashboard"
-import SuperCat from "../../components/supercategory/SuperCat"
-import SubCat from "../../components/subcategory/SubCat"
-import Category from "../../components/category/Category"
-
-
-
 const Admin = () => {
   return (
     <div className='admin'>
       <Sidebar/>
-      <Routes>
-       <Route path='addproduct' element={<AddProduct/>}/>
-       <Route path='listproduct' element={<ListProduct/>}/>
-       <Route path='dashboard' element={<Dashboard/>}/>
-       <Route path='supercategory' element={<SuperCat/>}/>
-       <Route path='category' element={<Category/>}/>
-       <Route path='subcategory' element={<SubCat/>}/>
-       
 
-
-      </Routes>
-      
+      <div className='admin-content'>
+        <Routes>
+          <Route path='addproduct' element={<AddProduct/>}/>
+          <Route path='listproduct' element={<ListProduct/>}/>
+          <Route path='dashboard' element={<Dashboard/>}/>
+          <Route path='supercategory' element={<SuperCat/>}/>
+          <Route path='category' element={<Category/>}/>
+          <Route path='subcategory' element={<SubCat/>}/>
+        </Routes>
+      </div>
     </div>
   )
 }
-
-export default Admin
