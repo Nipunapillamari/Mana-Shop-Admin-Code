016bcc79a6350b3f23cd9750d67f58ef744ef752
Nipunapillamari <nipunapillamari@gmail.com>
2026-04-30 22:23:28 +0530

    final build successfull

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import Sidebar from '../../components/sidebar'
+import Sidebar from '../../components/sidebar/Sidebar'
 import AddProduct from '../../components/addproduct'
 import Dashboard from '../../components/dashboard'
 import SuperCat from '../../components/supercategory'
